be0eb86d9efa6e223b963ecf1b7ae836b86e73d8
Andrew Rice <acr31@cam.ac.uk>
2013-11-17 19:24:53 +0000

    Chained the local stack trace onto the remote one when an error occurs

--- src/main/java/uk/ac/cam/cl/dtg/teaching/exceptions/ClientResponseFailureHandler.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/exceptions/ClientResponseFailureHandler.java
@@ -19,13 +19,55 @@ public class ClientResponseFailureHandler implements
 
 	@Override
 	public Response toResponse(ClientResponseFailure t) {
-		Throwable message = readException(t);
-		message.printStackTrace();
-		return Response.serverError().entity("API fail: " + message)
+		t.printStackTrace();
+		Throwable message = appendStackTrace(readException(t), t);
+		return Response.serverError().entity(message)
 				.type(MediaType.APPLICATION_JSON).build();
 	}
 
-	public static Throwable readException(ClientResponseFailure e) {
+	/**
+	 * Append the local stack trace onto the remote one. This then captures the
+	 * sequence of operations that happened before we caused the error on the
+	 * remote machine. This is different to using setCause because the remote
+	 * exception could well already have cause information which arose at the
+	 * remote site. There is no message to add from the local exception because
+	 * this handler only deals with ClientResponseFailures.
+	 * 
+	 * @param remoteException
+	 * @param localException
+	 * @return
+	 */
+	public static SerializableException appendStackTrace(
+			SerializableException remoteException,
+			ClientResponseFailure localException) {
+
+		SerializableStackTraceElement[] remoteStack = remoteException
+				.getSerializableStackTrace();
+		StackTraceElement[] localStack = localException.getStackTrace();
+		SerializableStackTraceElement[] newStack = new SerializableStackTraceElement[remoteStack.length
+				+ localStack.length];
+		int ptr = 0;
+		for (SerializableStackTraceElement s : remoteStack) {
+			newStack[ptr++] = s;
+		}
+		for (StackTraceElement s : localStack) {
+			newStack[ptr++] = new SerializableStackTraceElement(s);
+		}
+		remoteException.setSerializableStackTrace(newStack);
+		return remoteException;
+	}
+
+	/**
+	 * If a remote error occurs then the remote end should send a
+	 * SerializableException out over json. This method tries to recover that
+	 * object. If the content type isn't application/json we assume something
+	 * went very wrong at the other end and just package up the response in a
+	 * new Exception
+	 * 
+	 * @param e the ClientResponseFailure which we've caught
+	 * @return a SerializableException collected from the remote server
+	 */
+	public static SerializableException readException(ClientResponseFailure e) {
 		ClientResponse<?> clientResponse = e.getResponse();
 		String contentType = clientResponse.getHeaders().getFirst(
 				"Content-Type");
@@ -33,8 +75,9 @@ public class ClientResponseFailureHandler implements
 			// if they've sent us json then we'll assume they stuck with the API
 			// contract and have sent an ApiFailureMessage
 
-			Throwable message = (Throwable) clientResponse
+			SerializableException message = (SerializableException) clientResponse
 					.getEntity(SerializableException.class);
+
 			return message;
 		}
 
